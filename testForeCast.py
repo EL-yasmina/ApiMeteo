d95f0a78ec6d1e9ebdf02aba7d3bd95295c5509b
EL-Yasmina <yasminataif99@gmail.com>
2024-05-07 02:23:30 +0200

    📦 NEW: testé la fonction forecast avec pytest

---
+++ testForeCast.py
@@ -0,0 +1,15 @@
+import pytest
+from foreCast import get_weather_forecast
+
+def test_get_weather_forecast():
+    # Test case 1 vérifie si la fonction retourne le nombre attendu de jours de prévis
+    city = "Paris"
+    api_key = "61064c6295144de9b63101812242904"
+    days = 2
+    forecast_data = get_weather_forecast(city, api_key, days)
+    assert len(forecast_data) == days
+
+    # Test case 2 vérifie si la fonction retourne une liste vide pour une ville inexistante
+    city = "NonExistentCity"
+    forecast_data = get_weather_forecast(city, api_key, days)
+    assert forecast_data == []
